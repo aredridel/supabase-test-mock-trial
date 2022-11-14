eb5485379105d52d2660e6f3b6fbcd67683f28cd
Aria Stewart <aredridel@dinhe.net>
2022-11-13 21:33:57 -0500

    It fails right

--- __tests__/t.ts
+++ __tests__/t.ts
@@ -1,8 +1,16 @@
 import { supabaseStaging } from "../client";
 
+jest.mock("../client", () => ({
+  __esModule: true,
+  supabaseStaging: {
+    from: () => jest.fn({ select: () => jest.fn() }),
+  },
+}));
+
 describe("thing", () => {
 	it("works", async () => {
 		const {data, error} = await supabaseStaging.from('table-name').select('field1, field 2').eq('name', "thing");
-		console.log(data, error);
+		expect(error).toBeUndefined();
+		expect(data).toEqual([])
 	});
 });
