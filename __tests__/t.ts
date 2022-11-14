0acf1af8947c1d0432153900838f0195103c1b37
Aria Stewart <aredridel@dinhe.net>
2022-11-13 21:47:27 -0500

    It ... works? Badly.

--- __tests__/t.ts
+++ __tests__/t.ts
@@ -3,12 +3,23 @@ import { supabaseStaging } from "../client";
 jest.mock("../client", () => ({
   __esModule: true,
   supabaseStaging: {
-    from: () => jest.fn({ select: () => jest.fn() }),
-  },
+	from: jest.fn(() => {
+	  return {
+		select: jest.fn(() => {
+		  return {
+			eq: jest.fn(() => {
+			  return {data: []};
+			})
+		  }
+		})
+	  }
+	})
+  }
 }));
 
 describe("thing", () => {
 	it("works", async () => {
+
 		const {data, error} = await supabaseStaging.from('table-name').select('field1, field 2').eq('name', "thing");
 		expect(error).toBeUndefined();
 		expect(data).toEqual([])
